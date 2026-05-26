9cf5a28482e1c5a5f2941acf85d46ab143265a20
MAX <max@roslaniec.com>
2026-05-26 00:51:19 -0300

    feat: adiciona 6 cards de mockup e icones circulares das redes sociais no footer

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -20,6 +20,42 @@ const mockRestaurants = [
     avaliacao: 4.6,
     descricao: 'As melhores massas e pizzas da cidade, preparadas com ingredientes importados da Itália e receitas tradicionais de família.',
     capa: 'https://images.unsplash.com/photo-1551183053-bf91a1d81141?q=80&w=1000&auto=format&fit=crop'
+  },
+  {
+    id: 3,
+    titulo: 'La Dolce Vita',
+    destaque: false,
+    tipo: 'Italiana',
+    avaliacao: 4.6,
+    descricao: 'As melhores massas e pizzas da cidade, preparadas com ingredientes importados da Itália e receitas tradicionais de família.',
+    capa: 'https://images.unsplash.com/photo-1551183053-bf91a1d81141?q=80&w=1000&auto=format&fit=crop'
+  },
+  {
+    id: 4,
+    titulo: 'Hioki Sushi',
+    destaque: false,
+    tipo: 'Japonesa',
+    avaliacao: 4.9,
+    descricao: 'Peças exclusivas preparadas pelos nossos sushimen. Ingredientes frescos e selecionados para garantir a melhor experiência gastronômica.',
+    capa: 'https://images.unsplash.com/photo-1579871494447-9811cf80d66c?q=80&w=1000&auto=format&fit=crop'
+  },
+  {
+    id: 5,
+    titulo: 'La Dolce Vita',
+    destaque: false,
+    tipo: 'Italiana',
+    avaliacao: 4.6,
+    descricao: 'As melhores massas e pizzas da cidade, preparadas com ingredientes importados da Itália e receitas tradicionais de família.',
+    capa: 'https://images.unsplash.com/photo-1551183053-bf91a1d81141?q=80&w=1000&auto=format&fit=crop'
+  },
+  {
+    id: 6,
+    titulo: 'La Dolce Vita',
+    destaque: false,
+    tipo: 'Italiana',
+    avaliacao: 4.6,
+    descricao: 'As melhores massas e pizzas da cidade, preparadas com ingredientes importados da Itália e receitas tradicionais de família.',
+    capa: 'https://images.unsplash.com/photo-1551183053-bf91a1d81141?q=80&w=1000&auto=format&fit=crop'
   }
 ]
 
